e5746d270d827bb3f0d48a2b798f7d144a89d30e
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-09-04 22:37:01 +0530

    added db configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -19,6 +19,9 @@ export default function Home() {
       </header>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className=" text-xl">Example page</h1>
+        <div>
+          {data?.map((data) => <div key={data.id}>{data.content}</div>)}
+        </div>
       </main>
     </>
   );
