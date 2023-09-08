17a58a10d9af3817493e0a09e7999067a82350a8
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-09-08 23:19:57 +0530

    added pfp and create post

---
+++ src/components/CreatePostWidget.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+
+const CreatePostWidget = () => {
+  const { user } = useUser();
+  return (
+    <>
+      <div className=" flex p-6 border-b border-b-slate-600 sticky items-center">
+        {/* <UserButton afterSignOutUrl="/" /> */}
+        {user && user.imageUrl ? (
+          <img src={user?.imageUrl} alt="pfp" className=" w-[3rem] rounded-full" />
+        ) : (
+          <div>Loading..</div>
+        )}
+
+        <textarea
+          placeholder="What's up bro ?!"
+          className=" textarea textarea-bordered w-full max-w-lg mx-auto bg-slate-900 text-slate-300 text-lg "
+        />
+      </div>
+    </>
+  );
+};
+
+export default CreatePostWidget;
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,27 @@
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs/dist/types/server-helpers.server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserForClient = (user: User) => {
+  return { id: user.id, username: user.username, profilePicture: user.imageUrl }
+}
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    // geting userIds generated from oAuth on Clerk using Clerk client
+    const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })).map(filterUserForClient);
+
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id == post.authorId),
+    }));
   }),
 });
